6346e4bad66cbc6ff0294724a1704cf2480bb24c
muralikrishnagunja16-bit <muralikrishnagunja16@gmail.com>
2026-02-15 14:18:00 +0530

    Create Area_and_Perimeter_of_a_Rectangle.py

---
+++ Basic_problem_solving/Area_and_Perimeter_of_a_Rectangle.py
@@ -0,0 +1,6 @@
+a=float(input("length of the rectangle:"))
+b=float(input("breath of the Rectangle:"))
+area=a*b
+perimeter=2*(a+b)
+print("area of rectangle: ",area)
+print("perimeter of rectangle:",perimeter)
